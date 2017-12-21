7d1d388e41d7b4a65e3b91083ef9fe3c9f6a9a7b
Bruno <fernando23296@gmail.com>
2017-12-21 13:21:52 -0400

    Unpacking a Sequence into Separate Variables

---
+++ 1-1.py
@@ -0,0 +1,16 @@
+#Unpacking a Sequence into Separate Variables
+
+#PROBLEM
+#You have an N-element tuple or sequence that you would like to unpack into a collection of N variables
+p=(4,5)
+x,y=p
+print (x)
+print (y)
+#----------------
+data = ['acme', 50, 91.1 , (2013,12,21)]
+name, age, price, date = data
+print (name)
+print (age)
+
+
+
