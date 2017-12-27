6326fa40d08c34cfa00e7198c1260060426eea68
Bruno <fernando23296@gmail.com>
2017-12-27 10:58:04 -0400

    adding new examples

--- 1-2.py
+++ 1-2.py
@@ -1,1 +1,35 @@
 #1.2 Unpacking elements from iterables of arbitrary length
+#an example would be the humbers of a contact, for example there is a guy who works in a bank, so he have cellphone number, telephone, office phone
+
+record = ('dave', 'dave@example.com','232323', '4464')
+#always use * if you  want to do this
+
+name, email, *phone = record
+print (name)
+print (email)
+print (phone)
+
+#-------------------
+#an invert way
+
+*group, unit = [10,9,8,7,6,5,4,3,2,1]
+print(group)
+print(unit)
+#discussion
+
+line = 'nobody:*:-2:-2:Unpriviliged User:/var/empty:/usr/bin/false'
+uname, *fields,homedir, sh = line.split(':')
+print(uname)
+print(fields)
+print(homedir)
+print(sh)
+
+#simple example
+
+line='a:*:b:2:c'
+uno,dos,*tres =line.split(':')
+print(uno)
+print(dos)
+print(tres)
+
+
