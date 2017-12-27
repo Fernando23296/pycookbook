e76d5617a67b1898d2ab6e720eb53586e4969427
Bruno <fernando23296@gmail.com>
2017-12-27 12:38:42 -0400

    adding graphic example and the pop function

--- 1-3.py
+++ 1-3.py
@@ -2,11 +2,48 @@
 
 from collections import deque
 q = deque(maxlen=3)
+#in the maxlen we define the size
+q.append(1)
+q.append(2)
+q.append(3)
+
+
+print(q)
+'''
+________________________
+|       |       |       |
+|   1   |   2   |   3   |
+|       |       |       |
+________________________
+'''
+q.append(4)
+print(q)
+'''
+________________________
+|       |       |       |
+|   2   |   3   |   4   |
+|       |       |       |
+________________________
+
+the first value get removed
+
+'''
+q.append(5)
+print(q)
+
+#if you dont give it a maximum size, you get an UNBOUNDED queue
+q= deque()
 q.append(1)
 q.append(2)
 q.append(3)
 print(q)
-q.append(4)
+q.appendleft(4)
 print(q)
-q.append(5)
+#pop show and delete the last item
+print(q.pop())
 print(q)
+print(q.popleft())
+print(q)
+
+
+
