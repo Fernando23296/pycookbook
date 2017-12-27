a875877613f9762947ededc2bad9c5d88aed8661
Bruno <fernando23296@gmail.com>
2017-12-27 12:16:13 -0400

    adding the third recipe of the chapter one

---
+++ 1-3.py
@@ -0,0 +1,12 @@
+#in this case, deque is like a solid list, where the values added are going to move to the left
+
+from collections import deque
+q = deque(maxlen=3)
+q.append(1)
+q.append(2)
+q.append(3)
+print(q)
+q.append(4)
+print(q)
+q.append(5)
+print(q)
